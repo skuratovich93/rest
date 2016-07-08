c756f58c9c0e9ab43df9879dab3879068b87c7f9
y.skuratovich@itransition.com <75195328>
2016-07-08 13:49:51 +0300

    adding rest service for users

--- src/main/java/com/rest/controller/UserController.java
+++ src/main/java/com/rest/controller/UserController.java
@@ -3,7 +3,11 @@ package com.rest.controller;
 import com.rest.domain.User;
 import com.rest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +23,31 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greeting";
+    @RequestMapping("/user")
+    public ResponseEntity<List<User>> allUsers() {
+        List<User> users = (List<User>) userRepository.findAll();
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable Long userId) {
+        return userRepository.findOne(userId);
+    }
+
+    @RequestMapping(value = "/user/{userId}", method = RequestMethod.DELETE)
+    public ResponseEntity<List<User>> deleteUser(@PathVariable Long userId) {
+
+        User user = userRepository.findOne(userId);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            userRepository.delete(userId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
 }
