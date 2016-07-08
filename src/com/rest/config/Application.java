f51cfa202d87c1db20c7b4dece3b05e1182b86f6
y.skuratovich@itransition.com <75195328>
2016-07-08 10:07:21 +0300

    initial commit

---
+++ src/com/rest/config/Application.java
@@ -0,0 +1,16 @@
+package com.rest.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author y.skuratovich
+ * @since 08.07.2016
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/com/rest/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author y.skuratovich
+ * @since 08.07.2016
+ */
+@RestController
+public class UserController {
+
+}
